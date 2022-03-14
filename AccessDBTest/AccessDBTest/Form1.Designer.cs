195001856b3526c56b2efd95fbe84241f00b3fce
mustafaba0 <91112982+mustafaba0@users.noreply.github.com>
2022-03-14 11:45:00 +0300

    Add files via upload

---
+++ AccessDBTest/AccessDBTest/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace AccessDBTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.txtNumara = new System.Windows.Forms.TextBox();
+            this.txtSoyad = new System.Windows.Forms.TextBox();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEnter);
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(266, 114);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(87, 23);
+            this.btnEkle.TabIndex = 1;
+            this.btnEkle.Text = "Veri Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // txtNumara
+            // 
+            this.txtNumara.Location = new System.Drawing.Point(312, 88);
+            this.txtNumara.Name = "txtNumara";
+            this.txtNumara.Size = new System.Drawing.Size(100, 20);
+            this.txtNumara.TabIndex = 12;
+            // 
+            // txtSoyad
+            // 
+            this.txtSoyad.Location = new System.Drawing.Point(313, 64);
+            this.txtSoyad.Name = "txtSoyad";
+            this.txtSoyad.Size = new System.Drawing.Size(100, 20);
+            this.txtSoyad.TabIndex = 11;
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(313, 38);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(100, 20);
+            this.txtAd.TabIndex = 10;
+            // 
+            // txtId
+            // 
+            this.txtId.Enabled = false;
+            this.txtId.Location = new System.Drawing.Point(313, 13);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(100, 20);
+            this.txtId.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(263, 91);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Numara";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(267, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Soyad:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(284, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Ad:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(286, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "ID:";
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Location = new System.Drawing.Point(266, 143);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(87, 23);
+            this.btnGuncelle.TabIndex = 1;
+            this.btnGuncelle.Text = "Veri Güncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(266, 172);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(87, 23);
+            this.btnSil.TabIndex = 1;
+            this.btnSil.Text = "Veri Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(456, 262);
+            this.Controls.Add(this.txtNumara);
+            this.Controls.Add(this.txtSoyad);
+            this.Controls.Add(this.txtAd);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.TextBox txtNumara;
+        private System.Windows.Forms.TextBox txtSoyad;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Button btnSil;
+    }
+}
+
---
+++ AccessDBTest/AccessDBTest/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+namespace AccessDBTest
+{
+    public partial class Form1 : Form
+    {
+        OleDbConnection connection = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=db.accdb");
+        DataTable table;
+        string tableName = "Tablo1";
+        void Re(string tableName)
+        {
+            table = new DataTable();
+            try { new OleDbDataAdapter("SELECT * FROM " + tableName, connection).Fill(table); dataGridView1.DataSource = table; }
+            catch (Exception err) {}
+        }
+        void executeSql(string sqlString)
+        {
+            new OleDbCommand() { Connection = connection, CommandText = sqlString }.ExecuteNonQuery();
+
+        }
+        void addData(string tableName, Dictionary<string, string> data)
+        {
+            string sqlString = "INSERT INTO " + tableName + " (";
+            foreach (var item in data)
+            {
+                sqlString += ("" + item.Key + ",");
+            }
+            sqlString = sqlString.TrimEnd(',') + ") VALUES (";
+            foreach (var item in data)
+            {
+                sqlString += ("'" + item.Value + "',");
+            }
+            sqlString = sqlString.TrimEnd(',') + ")";
+            executeSql(sqlString);
+        }
+        void deleteData(string tableName, Dictionary<string, string> condition)
+        {
+            if (condition.Keys.Count < 1) return;
+            string sqlString = "DELETE FROM " + tableName + " ";
+            if (condition[condition.Keys.ElementAt(0)] != "*"){
+                sqlString += "WHERE ";
+                foreach (var item in condition)
+                {
+                    sqlString += (item.Key + "='"+item.Value+"' and");
+                }
+                sqlString = sqlString.TrimEnd('d').TrimEnd('n').TrimEnd('a');
+            }
+            executeSql(sqlString);
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            Re(tableName);
+        }
+
+        private void dataGridView1_CellEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            var selected = dataGridView1.CurrentRow.Cells;
+            txtId.Text = (string)selected[0].Value;
+            txtAd.Text = (string)selected[1].Value;
+            txtSoyad.Text = (string)selected[2].Value;
+            txtNumara.Text = (string)selected[3].Value;
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            addData(tableName, new Dictionary<string, string>() { { "Ad", txtAd.Text }, { "Soyad", txtSoyad.Text }, { "Numara", txtNumara.Text } });
+        }
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            Dictionary<string, string> condition = new Dictionary<string, string>() { { "Kimlik", txtId.Text } };
+            Dictionary<string, string> data = new Dictionary<string, string>() { { "Ad", txtAd.Text }, { "Numara", txtNumara.Text }, { "Soyad", txtSoyad.Text } };
+            foreach (var item in data)
+            {
+                if (item.Key != "Ad" && item.Value == "") throw new MissingPrimaryKeyException();
+            }
+            if (txtAd.Text == "*") condition["Kimlik"] = txtAd.Text;
+            deleteData(tableName, condition);
+        }
+    }
+}
